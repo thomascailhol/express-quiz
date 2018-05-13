46d72cf6ad3c7de3410d3c062436c85caeff5228
François-Julien Alcaraz <fj@mokriya.com>
2018-05-13 04:39:28 +0200

    change EJS to mustache + reformat app.js + comment app.js

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// NPM Dependencies
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const createError = require('http-errors');
+const mustacheExpress = require('mustache-express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// We are using the dotenv library to load our environmental variables from the .env file.
+dotenv.load();
 
+// Router
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+// Express
 var app = express();
 
-// view engine setup
+// View engine setup
+app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'mustache');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(session({
+  secret: 'shhhhhhhhh',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  //https://github.com/jshttp/http-errors
+  next(createError.NotFound());
 });
 
 // error handler
