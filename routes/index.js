55b15fd971f8e161aa82572977a080aa3c2618bc
François-Julien Alcaraz <fj@mokriya.com>
2018-05-13 04:39:45 +0200

    add routes for auth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Quiz' });
+});
+
+router.get('/login', function(req, res) {
+    res.send('You are on the login page');
+});
+
+router.get('/logout', function(req, res) {
+    res.send('You are on the logout page');
+});
+
+router.get('/polls', function(req, res) {
+    res.send('You are on the polls page');
+});
+
+router.get('/user', function(req, res) {
+    res.send('You are on the user page');
 });
 
 module.exports = router;
